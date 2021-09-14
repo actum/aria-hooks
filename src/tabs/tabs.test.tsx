dd0608eacc771218874873fda8c72af926b84ff1
Matej Vykoukal <matej.vykoukal@actumdigital.com>
2021-09-14 12:04:49 +0200

    Add tests for hook

--- src/tabs/tabs.test.tsx
+++ src/tabs/tabs.test.tsx
@@ -1,13 +1,78 @@
 import * as React from 'react';
-import { render, fireEvent } from '@testing-library/react';
+import { render, fireEvent, act } from '@testing-library/react';
+import { Tabs } from './tabs.stories';
 
 describe('Tests for useAriaNavigation', () => {
-  // it('should render button and tooltip', () => {
-  //   const { getByRole, container } = render(<Tooltip {...({} as any)} />);
-  //   const button = getByRole('button');
-  //   const tooltip = container.querySelector('#tooltip_tooltip');
-  //   expect(button).toBeInTheDocument();
-  //   expect(tooltip).toBeInTheDocument();
-  //   expect(tooltip.getAttribute('role')).toBe('tooltip');
+  it('should render tabs', () => {
+    const { getAllByRole, getByRole } = render(<Tabs {...({} as any)} />);
+
+    const tablist = getByRole('tablist');
+    const tabs = getAllByRole('tab');
+    const tabpanels = getAllByRole('tabpanel');
+
+    expect(tablist).toBeInTheDocument();
+
+    tabs.forEach((item) => {
+      expect(item).toBeInTheDocument();
+    });
+    tabpanels.forEach((item) => {
+      expect(item).toBeInTheDocument();
+    });
+  });
+
+  it('should exclude every tab from tab sequence, except first one', () => {
+    const { getAllByRole } = render(<Tabs {...({} as any)} />);
+
+    const tabs = getAllByRole('tab');
+
+    tabs.forEach((tab, idx) => {
+      if (idx === 0) {
+        expect(tab.tabIndex).toBe(0);
+      } else {
+        expect(tab.tabIndex).toBe(-1);
+      }
+    });
+  });
+
+  it('should move focus to next tab/first tab(if current focused tab is last tab) when right arrow key is pressed', () => {
+    const { getAllByRole } = render(<Tabs {...({} as any)} />);
+
+    const tabs = getAllByRole('tab');
+
+    act(() => {
+      tabs[0].focus();
+    });
+
+    fireEvent.keyDown(window, { key: 'ArrowRight' });
+
+    expect(tabs[1]).toBe(document.activeElement);
+  });
+  it('same test as above', () => {
+    const { getAllByRole } = render(<Tabs {...({} as any)} />);
+
+    const tabs = getAllByRole('tab');
+
+    act(() => {
+      tabs[0].focus();
+    });
+
+    fireEvent.keyDown(window, { key: 'ArrowRight' });
+
+    expect(tabs[1]).toBe(document.activeElement);
+  });
+
+  // it('should move focus to first element when Home key is pressed', () => {
+  //   const { getAllByRole } = render(<Tabs {...({} as any)} />);
+
+  //   const tabs = getAllByRole('tab');
+
+  //   act(() => {
+  //     tabs[0].focus();
+  //   });
+
+  //   fireEvent.keyDown(window, { key: 'ArrowRight' });
+  //   // fireEvent.keyDown(window, { key: 'Home' });
+
+  //   expect(tabs[1]).toBe(document.activeElement);
   // });
 });
