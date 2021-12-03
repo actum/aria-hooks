90203718374375dea2cc00d049edf9d187d90111
Matej Vykoukal <61636415+MatejVykoukal@users.noreply.github.com>
2021-12-03 11:19:15 +0100

    Polish accordion styles

--- src/hooks/accordion/accordion.styles.ts
+++ src/hooks/accordion/accordion.styles.ts
@@ -1,43 +1,74 @@
 import styled from 'styled-components';
-import { UniqueBtn } from '../../styles/sharedComponents';
+import { colorTheme } from '../../styles/themes';
+import {
+  borders,
+  colors,
+  fontSizes,
+  fontWeights,
+  spacers,
+  transitions,
+} from '../../styles/variables';
 
 export const Container = styled.div`
+  max-width: 500px;
   display: flex;
   flex-direction: column;
   align-items: center;
+  margin-inline: auto;
+
   text-align: left;
 `;
 
-export const StyledBtn = styled(UniqueBtn)`
-  border-radius: 0;
-  padding: 11px 15px;
+export const StyledTabBtn = styled.button`
   width: 100%;
-  max-width: 500px;
   display: flex;
+  align-items: center;
   justify-content: space-between;
-  .btn__icon {
-    transform: rotate(-90deg);
+  margin-bottom: ${spacers['spacer-2']};
+  padding: ${spacers.defaultButtonPadding};
+
+  background-color: ${(props: { theme: colorTheme }) =>
+    props.theme.secondaryBackground};
+  color: ${(props: { theme: colorTheme }) => props.theme.textColor};
+  font-weight: ${fontWeights.bold};
+  font-size: ${fontSizes.m};
+  outline: none;
+  cursor: pointer;
+  border: ${borders.defaultWidth} solid ${colors.black};
+
+  transition-property: ${transitions.buttonProperties};
+  transition-timing-function: ${transitions.timingFunction};
+  transition-duration: ${transitions.defaultDuration};
+
+  &:hover {
+    color: ${colors.red};
+    border-color: ${colors.red};
   }
 
-  &:hover,
   &:focus {
+    box-shadow: ${borders.focusOutline};
+  }
+
+  &[aria-expanded='true'] {
+    margin-bottom: 0;
     .btn__icon {
       transform: rotate(-90deg);
     }
   }
 
-  &:not([aria-expanded='true']) {
-    margin-bottom: 11px;
-    .btn__icon {
-      transform: rotate(90deg);
-    }
+  .btn__icon {
+    vertical-align: middle;
+    margin-left: ${spacers['spacer-6']};
+
+    fill: ${colors.red};
+    transform: rotate(90deg);
   }
 `;
 
 export const StyledPanel = styled.div`
   width: 100%;
-  max-width: 500px;
-  font-size: 16px;
-  padding: 11px 16px;
-  font-family: Roboto, sans-serif;
+  padding: ${spacers['spacer-3']} ${spacers['spacer-4']};
+
+  font-size: ${fontSizes.m};
+  color: ${(props: { theme: colorTheme }) => props.theme.textColor};
 `;
