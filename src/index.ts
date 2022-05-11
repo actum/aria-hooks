3a14dbe2ef38fbc4f9bd03989325365baca28a60
Matej Vykoukal <61636415+MatejVykoukal@users.noreply.github.com>
2022-05-11 15:25:10 +0200

    Add missing hooks exports

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,6 @@
+export { useAriaAccordion } from './hooks/accordion';
+export { useAriaListbox } from './hooks/listbox';
 export { useAriaModal } from './hooks/modal';
 export { useAriaNavigation } from './hooks/navigation';
+export { useAriaTabs } from './hooks/tabs';
 export { useAriaToolTip } from './hooks/tooltip';
-export { useAriaListbox } from './hooks/listbox';
